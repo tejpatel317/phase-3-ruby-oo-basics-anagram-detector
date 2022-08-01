f542cd692bc5bf1473d9f0cbd24b2b980b8c2d22
Tej Patel <tejpatel317@gmail.com>
2022-08-01 19:54:47 -0400

    commit tree e14f2d1293736b765840e8ca1025c5eef0aa8ec7

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array_of_strings)
+        newArray = array_of_strings.filter do |str|
+            str.chars.sort == @word.chars.sort
+        end
+        newArray
+    end
+end
